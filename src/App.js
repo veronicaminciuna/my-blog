6d52b84bf54e55289cf1efdcb4e90692a7ad7958
Veronica Minciuna <veronicaminci@gmail.com>
2022-04-15 12:33:35 -0400

    routes

--- src/App.js
+++ src/App.js
@@ -6,14 +6,23 @@ import NewPost from './NewPost';
 import PostPage from './PostPage';
 import About from './About';
 import Missing from './Missing';
-import {Route, Switch, useHistory} from 'react-router-dom';
+import {Route, Routes, useNavigate} from 'react-router-dom';
 import {useState, useEffect} from 'react';
 
 
 function App() {
   return (
-    <div className="App">
-
+    <div className='App'>
+      <Header/>
+      <Nav/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/post" element={<NewPost />} />
+        <Route path="/post/:id" element={<PostPage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<Missing />} />
+      </Routes>
+      <Footer/>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -8,9 +8,10 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <Routes>
-    <Route path='/' element={<App />} />
-    </Routes>
+      <Routes>
+        <Route path="/*" element={ <App /> }>
+        </Route>
+      </Routes>
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
